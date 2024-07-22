3706091b22b8f62e61dc9b0f04e9ff2a2967a972
İbrahim Kaymak <ibrahimkaymak817@outlook.com>
2024-07-22 16:04:09 +0300

    Destination Details

---
+++ DreamTravel/Controllers/DestinationController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DreamTravel.Controllers
+{
+    public class DestinationController : Controller
+    {
+        GenericManager<Destination> manager = new GenericManager<Destination>(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = manager.GetListAll();
+            return View(values);
+        }
+        public IActionResult DestinationDetails(int id)
+        {
+            var did = manager.GetById(id);
+            return View(did);
+        }
+    }
+}
--- EntityLayer/Concrete/Destination.cs
+++ EntityLayer/Concrete/Destination.cs
@@ -15,6 +15,18 @@ namespace EntityLayer.Concrete
         public string? DayNight { get; set; }
         public double Price { get; set; }
         public string? Description { get; set; }
+        public string? CoverImage { get; set; }
+        public string? BlogImage1 { get; set; }
+        public string? BlogImage2 { get; set; }
+        public string? Description1 { get; set; }
+        public string? Description2 { get; set; }
+        public string? Description3 { get; set; }
+        public string? Description4 { get; set; }
+        public string? DescriptionTitle1 { get; set; }
+        public string? DescriptionTitle2 { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? GuideName { get; set; }
+        public string? GuideImage { get; set; }
         public string? Image { get; set; }
         public int Capacity { get; set; }
         public bool Status { get; set; }
