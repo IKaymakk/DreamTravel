e47d531d7a439d0c74f108976aba25399db9d397
İbrahim Kaymak <ibrahimkaymak817@outlook.com>
2024-08-08 14:23:59 +0300

    Lang

---
+++ DreamTravel/ViewComponents/Member/_MemberLayoutNavbar.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DreamTravel.ViewComponents.Member
+{
+    public class _MemberLayoutNavbar : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _MemberLayoutNavbar(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            ViewBag.navimage = user.ImageUrl;
+            ViewBag.navname = user.Name;
+
+            return View();
+        }
+    }
+}
